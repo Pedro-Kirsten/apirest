9897249ccef2b7d4149f54f2a2b48dd4ad005ed2
Pedro-Kirsten <91696460+Pedro-Kirsten@users.noreply.github.com>
2023-06-03 14:06:42 -0300

    Add files via upload

---
+++ src/main/java/com/example/api/model/Produto.java
@@ -0,0 +1,96 @@
+package com.example.api.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Produto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   
+    @Column (nullable = false)
+    private Long idProduto;
+    
+    @Column(nullable = false)
+    private String nomeProduto;
+    
+    @Column(nullable = false)
+    private String Marca;
+    
+    @Column(nullable = false)
+    private double valor;
+    
+    @Column(nullable = false)
+    private String descricao;
+
+    public long getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(long idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + (int) (this.idProduto ^ (this.idProduto >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Produto other = (Produto) obj;
+        return this.idProduto == other.idProduto;
+    }
+    
+    
+}
